4d5ea91cff45893c6288a7fab32ea3ff1eae4c6c
Mirche Toshevski <mirche.toshevski@gmail.com>
2016-05-31 00:29:22 +0200

    first commit

--- Prototyper/Form1.Designer.cs
+++ Prototyper/Form1.Designer.cs
@@ -24,11 +24,158 @@
         /// </summary>
         private void InitializeComponent() {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.imageRightClickMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bringToTopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.imageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel = new System.Windows.Forms.Panel();
+            this.zoomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoomInToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoomOutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.imageRightClickMenu.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFileDialog1";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.ContextMenuStrip = this.imageRightClickMenu;
+            this.pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox.Image")));
+            this.pictureBox.Location = new System.Drawing.Point(257, 66);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(186, 114);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // imageRightClickMenu
+            // 
+            this.imageRightClickMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deleteToolStripMenuItem,
+            this.bringToTopToolStripMenuItem});
+            this.imageRightClickMenu.Name = "imageRightClickMenu";
+            this.imageRightClickMenu.Size = new System.Drawing.Size(142, 48);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // bringToTopToolStripMenuItem
+            // 
+            this.bringToTopToolStripMenuItem.Name = "bringToTopToolStripMenuItem";
+            this.bringToTopToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.bringToTopToolStripMenuItem.Text = "Bring To Top";
+            this.bringToTopToolStripMenuItem.Click += new System.EventHandler(this.bringToTopToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.imageToolStripMenuItem,
+            this.zoomToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(938, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // imageToolStripMenuItem
+            // 
+            this.imageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.imageToolStripMenuItem.Name = "imageToolStripMenuItem";
+            this.imageToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.imageToolStripMenuItem.Text = "Image";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // panel
+            // 
+            this.panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel.AutoScroll = true;
+            this.panel.Location = new System.Drawing.Point(0, 27);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(938, 440);
+            this.panel.TabIndex = 3;
+            // 
+            // zoomToolStripMenuItem
+            // 
+            this.zoomToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zoomInToolStripMenuItem,
+            this.zoomOutToolStripMenuItem});
+            this.zoomToolStripMenuItem.Name = "zoomToolStripMenuItem";
+            this.zoomToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.zoomToolStripMenuItem.Text = "Zoom";
+            // 
+            // zoomInToolStripMenuItem
+            // 
+            this.zoomInToolStripMenuItem.Name = "zoomInToolStripMenuItem";
+            this.zoomInToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.zoomInToolStripMenuItem.Text = "Zoom In";
+            this.zoomInToolStripMenuItem.Click += new System.EventHandler(this.zoomInToolStripMenuItem_Click);
+            // 
+            // zoomOutToolStripMenuItem
+            // 
+            this.zoomOutToolStripMenuItem.Name = "zoomOutToolStripMenuItem";
+            this.zoomOutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.zoomOutToolStripMenuItem.Text = "Zoom Out";
+            this.zoomOutToolStripMenuItem.Click += new System.EventHandler(this.zoomOutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(938, 469);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.imageRightClickMenu.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem imageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip imageRightClickMenu;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bringToTopToolStripMenuItem;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.ToolStripMenuItem zoomToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zoomInToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zoomOutToolStripMenuItem;
     }
 }
 
--- Prototyper/Form1.cs
+++ Prototyper/Form1.cs
@@ -7,11 +7,151 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace Prototyper {
     public partial class Form1 : Form {
+        Image img;
+        bool pictureBoxIsClicked;
+        private Point MouseDownLocation;
+        int mouseResizePosition; // 12 - top, 3 - right, 6 - bottom, 9 - left
+        bool mouseClickedForResize;
+        int pictureBoxSizeWidth;
+        int pictureBoxSizeHeight;
+        int zoomPanel = 1;
+
+
         public Form1() {
             InitializeComponent();
+            pictureBoxIsClicked = false;
+            mouseClickedForResize = false;
+            WindowState = FormWindowState.Maximized;
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e) {
+            if (openFile.ShowDialog() == DialogResult.OK) {
+                PictureBox pictureBox = new PictureBox();
+                pictureBox.Image = Image.FromFile(openFile.FileName);
+                pictureBox.Height = (int) ((500.0 / pictureBox.Image.Width) * pictureBox.Image.Height * 1.0);
+                pictureBox.Width = 500;
+                
+                pictureBox.Top = 20;
+                pictureBox.Left = (panel.Width - 500) / 2;
+
+                pictureBox.MouseDown += pictureBox_MouseDown;
+                pictureBox.MouseUp += pictureBox_MouseUp;
+                pictureBox.MouseMove += pictureBox_MouseMove;
+
+                pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox.ContextMenuStrip = imageRightClickMenu;
+
+                panel.Controls.Add(pictureBox);
+                pictureBox.BringToFront();
+            }
+        }
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e) {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left) {
+                MouseDownLocation = e.Location;
+                pictureBoxSizeWidth = (sender as PictureBox).Width;
+                pictureBoxSizeHeight = (sender as PictureBox).Height;
+                if (mouseResizePosition == 0) {
+                    pictureBoxIsClicked = true;
+                    mouseClickedForResize = false;
+                } else {
+                    pictureBoxIsClicked = false;
+                    mouseClickedForResize = true;
+                }
+            }
+        }
+
+        private void pictureBox_MouseUp(object sender, MouseEventArgs e) {
+            MouseDownLocation = Point.Empty;
+            pictureBoxIsClicked = false;
+            mouseClickedForResize = false;
+        }
+
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e) {
+            PictureBox pictureBox = sender as PictureBox;
+            checkResizer(pictureBox, e);
+            if (e.Button == System.Windows.Forms.MouseButtons.Left) {
+                if (pictureBoxIsClicked) {
+                    pictureBox.Left = e.X + pictureBox.Left - MouseDownLocation.X;
+                    pictureBox.Top = e.Y + pictureBox.Top - MouseDownLocation.Y;
+                } else if (mouseClickedForResize) {
+                    int w = e.X - MouseDownLocation.X + pictureBoxSizeWidth;
+                    if (w > 8)
+                        pictureBox.Width = w;
+                    int h = e.Y - MouseDownLocation.Y + pictureBoxSizeHeight;
+                    if (h > 8)
+                        pictureBox.Height = h;
+                }
+
+            }
+        }
+
+        private void checkResizer(PictureBox pb, MouseEventArgs e) {
+            if (mouseClickedForResize || pictureBoxIsClicked)
+                return;
+
+            int left = pb.Left;
+            int right = pb.Right;
+            int top = pb.Top;
+            int bottom = pb.Bottom;
+            int x = e.X;
+            int y = e.Y;
+
+            if (x > right - left - 8 && y > bottom - top - 8) {
+                Cursor.Current = Cursors.SizeNWSE;
+                mouseResizePosition = 4;
+            }
+            /*
+            if (x < 4) {
+                mouseResizePosition = 9;
+                Cursor.Current = Cursors.SizeWE;
+            } else if (x > right - left - 3) {
+                mouseResizePosition = 3;
+                Cursor.Current = Cursors.SizeWE;
+            } else if (y < 4) {
+                mouseResizePosition = 12;
+                Cursor.Current = Cursors.SizeNS;
+            } else if (y > bottom - top - 3) {
+                mouseResizePosition = 6;
+                Cursor.Current = Cursors.SizeNS;
+            } */else {
+                mouseResizePosition = 0;
+                Cursor.Current = Cursors.Default;
+            }
+        }
+
+        private bool checkBor(int a, int b) {
+            Debug.WriteLine(a + " : " + b);
+            if (Math.Abs(a - b) < 4) {
+                return true;
+            }
+            return false;
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e) {
+            PictureBox pb = (((sender as ToolStripItem).Owner as ContextMenuStrip).SourceControl as PictureBox);
+            pb.Dispose();
+        }
+
+        private void bringToTopToolStripMenuItem_Click(object sender, EventArgs e) {
+            PictureBox pb = (((sender as ToolStripItem).Owner as ContextMenuStrip).SourceControl as PictureBox);
+            pb.BringToFront();
+        }
+
+        private void zoomInToolStripMenuItem_Click(object sender, EventArgs e) {
+            foreach (Control c in panel.Controls) {
+                c.Scale(new SizeF(1.2f, 1.2f));
+            }
+        }
+
+        private void zoomOutToolStripMenuItem_Click(object sender, EventArgs e) {
+            foreach (Control c in panel.Controls) {
+                c.Scale(new SizeF(0.8f, 0.8f));
+            }
         }
     }
 }
